fbc9fd0d88398eea56fb2598793341a72bef38cc
Victoria Amaral <vicpantojadoamaral@gmail.com>
2018-06-18 04:14:50 -0300

    Organizei as pastas e fiz mudanças pra pagina rodar no django

--- nossos_membros/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NossosMembrosConfig(AppConfig):
-    name = 'nossos_membros'
--- reconjr/settings.py
+++ reconjr/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nossos_membros',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
