7723074c19a5e7cb6e05e918cb9768b242d96940
Victoria Amaral <vicpantojadoamaral@gmail.com>
2018-06-21 17:41:50 -0300

    fixing to pull request

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 
 # Create your views here.
