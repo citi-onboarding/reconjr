55cf66cf2d221725b65facb2a76e8ef9ba05aa2a
Victoria Amaral <vicpantojadoamaral@gmail.com>
2018-06-21 16:06:29 -0300

    Ajuste da responsividade

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from .models import *
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['membros'] = Membro.objects.all()
+        return context
